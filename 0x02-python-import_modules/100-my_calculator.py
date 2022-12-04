79e6d65004afea26e99b704fe78698254a48b85c
ochochejay <ochochejay@gmail.com>
2022-12-04 10:57:58 -0800

    correct

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     ac = len(sys.argv) - 1
     av = sys.argv
     op_list = ["+", "-", "*", "/"]
-    func_list =[add, sub, mul, div]
+    func_list = [add, sub, mul, div]
     if ac != 3:
         print("Usage: {:s} <a> <operator> <b>".format(av[0]))
         exit(1)
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     for i in range(len(op_list)):
         if av[2] == op_list[i]:
             ans = func_list[i](int(av[1]), int(av[3]))
-            print("{:s} {:s} {:s} = {:d}".format(av[1], av[2], av[3],ans))
+            print("{:s} {:s} {:s} = {:d}".format(av[1], av[2], av[3], ans))
             break
     else:
         print("Unknown operator. Available operators: +, -, * and /")
